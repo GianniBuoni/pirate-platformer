02bcc793cabd8ee478e952a4840284449cc515cd
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-01-01 14:59:52 -0500

    🧹 small tweaks and fixes
    - fixed issues with wall jumps and wall slides
    - added floor spikes to the damage sprites group

--- src/level/setup_functions/objects.py
+++ src/level/setup_functions/objects.py
@@ -3,7 +3,10 @@ from random import uniform
 from settings import *
 from sprites import *
 
-def setup(self, tmx_map, level_frames):
+from typing import TYPE_CHECKING
+if TYPE_CHECKING: from level import Level
+
+def setup(self: "Level", tmx_map, level_frames):
         # objects
     for obj in tmx_map.get_layer_by_name("Objects"):
         if obj.name == "player":
@@ -12,13 +15,15 @@ def setup(self, tmx_map, level_frames):
             sprites.Sprite((obj.x, obj.y), self.all_sprites, self.collision_sprites, surf = obj.image)
         else: # all other animated sprites on the objects layer
             frames = level_frames[obj.name]
-            groups = [self.all_sprites]
+            groups: list = [self.all_sprites]
             animation_speed = ANIMATION_SPEED
             z = Z_LAYERS["main"]
 
             # modify frames, groups, animation_speed, and z based on object name
-            if obj.name == "floor_spike" and obj.inverted:
-                frames = [pygame.transform.flip(x, False, True) for x in frames]
+            if obj.name == "floor_spike":
+                groups.append(self.damage_sprites)
+                if obj.inverted:
+                    frames = [pygame.transform.flip(x, False, True) for x in frames]
             if obj.name in ("palm_large", "palm_small"): groups.append(self.platform_sprites)
             if "palm" in obj.name: animation_speed += uniform(-1,1)
             if "bg" in obj.name: z = Z_LAYERS["bg"]
@@ -26,4 +31,4 @@ def setup(self, tmx_map, level_frames):
             animated.AnimatedSprite((obj.x, obj.y), frames, groups, z=z, animation_speed=animation_speed)
 
         if obj.name == "flag":
-            self.level_flag = pygame.FRect(obj.x, obj.y, obj.width, obj.height)
+            self.level_flag = pygame.FRect(obj.x, obj.y, obj.width, obj.height) # pyright: ignore
--- src/player/_move.py
+++ src/player/_move.py
@@ -10,16 +10,18 @@ def move(self: "Player", dt):
     self.collision("horizontal")
 
     # vert
-    match self.check_collision_side():
-        case CollidesWith.LEFT | CollidesWith.RIGHT:
+    if (
+        self.check_collision_side() == CollidesWith.LEFT
+        or self.check_collision_side() == CollidesWith.RIGHT
+        and not self.timers["wjump t/o"]
+    ):
         # wall slide decreases gravity
-            if not self.timers["wjump t/o"]:
-                self.direction.y = 0
-                self.hitbox.y += self.gravity / 10 * dt
-        case _: # normal gravity
-            self.direction.y += self.gravity / 2 * dt
-            self.hitbox.y += self.direction.y * dt
-            self.direction.y += self.gravity / 2 * dt
+        self.direction.y = 0
+        self.hitbox.y += self.gravity / 10 * dt
+    else: # normal gravity
+        self.direction.y += self.gravity / 2 * dt
+        self.hitbox.y += self.direction.y * dt
+        self.direction.y += self.gravity / 2 * dt
 
     self.collision("vertical")
 
@@ -35,9 +37,10 @@ def move(self: "Player", dt):
                 self.timers["wjump t/o"].activate()
                 self.direction.y = -self.jump_distance
             case CollidesWith.LEFT | CollidesWith.RIGHT:
-                self.timers["jump t/o"].activate()
+                if not self.timers["wjump t/o"]:
+                    self.timers["jump t/o"].activate()
+                    self.direction.x = 1 if self.check_collision_side() == CollidesWith.LEFT else -1
                 self.direction.y = -self.jump_distance
-                self.direction.x = 1 if CollidesWith.LEFT else -1
         self.jump = False
 
     self.rect.center = self.hitbox.center
