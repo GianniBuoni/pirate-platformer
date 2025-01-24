111e15ba312c0246f59c6ddbba500214df4e313e
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-01-24 16:41:55 -0500

    inital setup of raylib
    - flake set up for development!
    - dependecies for raylib declared
    - first build succeeded

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "A raylib dev shell and build environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -11,7 +11,7 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    gg = pkgs.writeShellScriptBin "gg" ''
+    gg = pkgs.writeShellScriptBin "ggg" ''
       go run .
     '';
   in {
@@ -22,10 +22,27 @@
         ]
         ++ (with pkgs; [
           go
+          libGL
           tiled
+
+          # x11 dependencies
+          xorg.libX11
+          xorg.libX11.dev
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXrandr
         ]);
 
-      shellHook = "tmux";
+      buildInputs = with pkgs; [
+        wayland
+        glfw-wayland
+        libxkbcommon
+      ];
+      shellHook = ''
+        export LD_LIBRARY_PATH=${pkgs.wayland}/lib:$LD_LIBRARY_PATH &&
+        tmux
+      '';
     };
   };
 }
