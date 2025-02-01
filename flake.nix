4374d8394c4301d713febe87918e53d5b9bdb62f
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-02-01 11:50:35 -0500

    allow unfree packages to evaluate for dev flake

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
     ggg = pkgs.writeShellScriptBin "ggg" ''
       go run -tags x11 .
     '';
