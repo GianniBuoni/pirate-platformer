efd160977481c8659d5e42b15e0eafd9318aad5c
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-02-14 13:31:48 -0500

    changes to level data
    - fixed broken tileset

--- data/tilesets/items.tsx
+++ data/tilesets/items.tsx
@@ -1,19 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="items" tilewidth="64" tileheight="64" tilecount="5" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="64" height="64" source="../../graphics/objects/items/diamond.png"/>
- </tile>
- <tile id="1">
-  <image width="64" height="64" source="../../graphics/objects/items/gold.png"/>
- </tile>
- <tile id="2">
-  <image width="64" height="64" source="../../graphics/objects/items/potion.png"/>
- </tile>
- <tile id="3">
-  <image width="64" height="64" source="../../graphics/objects/items/silver.png"/>
- </tile>
- <tile id="4">
-  <image width="64" height="64" source="../../graphics/objects/items/skull.png"/>
- </tile>
+<tileset version="1.10" tiledversion="1.11.1" name="items" tilewidth="64" tileheight="64" tilecount="6" columns="2">
+ <image source="../../graphics/tilesets/items.png" width="128" height="192"/>
+ <tile id="4" type="potion"/>
+ <tile id="1" type="silver"/>
+ <tile id="0" type="gold"/>
+ <tile id="3" type="diamond"/>
+ <tile id="2" type="skull"/>
+ <tile id="5"/>
 </tileset>
